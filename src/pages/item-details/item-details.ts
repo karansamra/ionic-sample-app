3f826a0aae44269bf67d64c11818daa4704ce041
Jsingh <johndoe@example.com>
2017-01-06 18:42:54 +0530

    Adding code.

---
+++ src/pages/item-details/item-details.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by jsingh7 on 1/5/2017.
+ */
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-item-details',
+    templateUrl: 'item-details.html'
+})
+export class ItemDetailsPage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
